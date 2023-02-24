458ca2fe19319c99076e36076e9a3e844ea0cde6
masafiedin <masafiedin24@gmail.com>
2023-02-24 21:01:40 +0200

    formatted page

--- diceGame.js
+++ diceGame.js
@@ -5,51 +5,48 @@ function randomDice() {
   return dice + 1;
 }
 //change dice image
-function changeImg(diceNo,div) {
-    console.log("This is diceNO",diceNo)
- 
+function changeImg(diceNo, div) {
+  console.log("This is diceNO", diceNo);
+
   switch (diceNo) {
     case 1: {
-       document.getElementById(div).src = "Resources/d1.png";
+      document.getElementById(div).src = "Resources/d1.png";
       break;
     }
     case 2: {
-        document.getElementById(div).src ="Resources/d2.png";
+      document.getElementById(div).src = "Resources/d2.png";
       break;
     }
     case 3: {
-        document.getElementById(div).src ="Resources/d3.png";
+      document.getElementById(div).src = "Resources/d3.png";
       break;
     }
     case 4: {
-        document.getElementById(div).src = "Resources/d4.png";
+      document.getElementById(div).src = "Resources/d4.png";
       break;
     }
     case 5: {
-        document.getElementById(div).src = "Resources/d5.png";
+      document.getElementById(div).src = "Resources/d5.png";
       break;
     }
     case 6: {
-        document.getElementById(div).src = "Resources/d6.png";
+      document.getElementById(div).src = "Resources/d6.png";
       break;
     }
   }
 }
 
 //game logic
-function play(){
-    var temp1 = randomDice();
-    var temp2 = randomDice();
-    changeImg(temp1,"p1");
-    changeImg(temp2,"p2");
-    if(temp1 >temp2){
-        document.getElementById("winner").innerHTML = "Player 1 wins!";
-    }
-    else if(temp2>temp1){
-        document.getElementById("winner").innerHTML = "Player 2 wins!"
-    }
-    else{
-        document.getElementById("winner").innerHTML = "Tie!"
-    }
-
+function play() {
+  var temp1 = randomDice();
+  var temp2 = randomDice();
+  changeImg(temp1, "p1");
+  changeImg(temp2, "p2");
+  if (temp1 > temp2) {
+    document.getElementById("winner").innerHTML = "Player 1 wins!";
+  } else if (temp2 > temp1) {
+    document.getElementById("winner").innerHTML = "Player 2 wins!";
+  } else {
+    document.getElementById("winner").innerHTML = "Tie!";
+  }
 }
